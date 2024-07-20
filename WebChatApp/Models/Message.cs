764da1797ba89f1f06d29166b9e1a96a5dca7688
colejnr <messicol190@gmail.com>
2024-07-20 21:09:33 +0100

    created model to handle data for the messages

---
+++ WebChatApp/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace WebChatApp.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public string TimeCreated { get; set; }
+
+        public Message() 
+        {
+            Id = Guid.NewGuid().ToString();
+            Content = string.Empty;
+            TimeCreated = DateTime.Now;
+        }
+    }
+}
