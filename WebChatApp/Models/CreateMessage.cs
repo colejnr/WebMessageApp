32df0bbeb5970342ddbc96e57522701d30b8cae5
colejnr <messicol190@gmail.com>
2024-07-20 20:03:40 +0100

    Created link page layout

---
+++ WebChatApp/Models/CreateMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebChatApp.Models 
+{ 
+
+    public class CreateMessage
+    {
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public string SecretKey { get; set; }
+
+        public CreateMessage() 
+        {
+            Message = string.Empty;
+            SecretKey = string.Empty;
+        }
+
+    }
+}
