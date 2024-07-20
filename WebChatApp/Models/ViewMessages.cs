5bb33451119969b8bbe86b16fca51c68001b0720
colejnr <messicol190@gmail.com>
2024-07-20 21:47:11 +0100

    Created model to view the content

---
+++ WebChatApp/Models/ViewMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace WebChatApp.Models
+{
+    public class ViewMessage
+    {
+        public string Id { get; set; }
+        public string EncryptedContent { get; set; }
+        public string DecryptedContent { get; set; }
+        public DateTime TimeCreated { get; set; }
+
+        public ViewMessage()
+        {
+            Id = Guid.NewGuid().ToString();
+            EncryptedContent = string.Empty;
+            DecryptedContent = string.Empty;
+            TimeCreated = DateTime.Now;
+        }
+
+    }
+}
