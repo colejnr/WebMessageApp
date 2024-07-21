49287cabda92d8ac954035bdd298b0d7426d215a
colejnr <messicol190@gmail.com>
2024-07-21 11:13:21 +0100

    Created Database

---
+++ WebChatApp/Data/Migrations/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebChatApp.Models;
+
+namespace WebChatApp.Data.Migrations
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
--- WebChatApp/Program.cs
+++ WebChatApp/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebChatApp.Data.Migrations;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDBContext>(); 
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
