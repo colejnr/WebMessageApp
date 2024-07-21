9ff9a1dbf320d2866fcbad43ccf1df6d7b219463
colejnr <messicol190@gmail.com>
2024-07-21 19:41:59 +0100

    Created database(Update)

---
+++ WebChatApp/Models/DecryptMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebChatApp.Models
+{
+    public class DecryptMessage
+    {
+        [Required]
+        public string SecretKey { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
+    }
+}
