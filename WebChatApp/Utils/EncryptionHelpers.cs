5031bb3e071856fa475de218cbbbe89053ee3bbb
RobinsSojan <163749930+RobinsSojan@users.noreply.github.com>
2024-07-21 15:49:48 +0100

    made changes in Decrypt method implementation in EncryptionHelper.cs

--- WebChatApp/Utils/EncryptionHelpers.cs
+++ WebChatApp/Utils/EncryptionHelpers.cs
@@ -50,3 +50,14 @@ namespace WebChatApp.Utils
 				aesAlg.Key = GetHashedKey(key);
 				aesAlg.IV = iv;
 
+				ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+				using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+				using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+				using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+				{
+					return srDecrypt.ReadToEnd();
+				}
+			}
+		}
+
