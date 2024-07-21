62ac1704c52ad26f7f7c34383662576335cad24f
RobinsSojan <163749930+RobinsSojan@users.noreply.github.com>
2024-07-21 15:44:45 +0100

    Implemented AES encryption with MemoryStream and CryptoStream in EncryptionHelper.cs

--- WebChatApp/Utils/EncryptionHelpers.cs
+++ WebChatApp/Utils/EncryptionHelpers.cs
@@ -14,5 +14,25 @@ namespace WebChatApp.Utils
 			{
 				aesAlg.Key = GetHashedKey(key);
 				aesAlg.GenerateIV();
+				ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+				using (MemoryStream msEncrypt = new MemoryStream())
+				{
+					using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+					using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+					{
+						swEncrypt.Write(rawMessage);
+					}
+
+					byte[] iv = aesAlg.IV;
+					byte[] encryptedContent = msEncrypt.ToArray();
+					byte[] combinedIvAndEncryptedContent = new byte[iv.Length + encryptedContent.Length];
+					Array.Copy(iv, 0, combinedIvAndEncryptedContent, 0, iv.Length);
+					Array.Copy(encryptedContent, 0, combinedIvAndEncryptedContent, iv.Length, encryptedContent.Length);
+
+					return Convert.ToBase64String(combinedIvAndEncryptedContent);
+				}
+			}
+		}
 
 
