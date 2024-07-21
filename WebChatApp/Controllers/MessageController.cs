82fcbaa3fa9b3c0ea829462cdef5828aa7b70684
Najeebvm <najeebvm9562@gmail.com>
2024-07-21 18:28:24 +0530

    add message controller

---
+++ WebChatApp/Controllers/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebChatApp.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        public readonly MessageController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+    }
+}
