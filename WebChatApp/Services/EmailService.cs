45c1856c99064c004d1809f16548723fcf9e6925
Najeebvm <najeebvm9562@gmail.com>
2024-07-22 23:01:12 +0530

    Implement SendEmailAsync method

--- WebChatApp/Services/EmailService.cs
+++ WebChatApp/Services/EmailService.cs
@@ -1,29 +1,41 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Threading.Tasks;
+using WebChatApp.Models;
 
-public class Class1
+
+namespace WebChatApp.Services
 {
 	public class EmailService
 	{
 		private readonly SmtpSettings _smtpSettings;
-	public EmailService(IOptions<SmtpSettings> smtpSettings)
+
+		public EmailService(IOptions<SmtpSettings> smtpSettings)
+		{
 			_smtpSettings = smtpSettings.Value;
-	
-	}
-	public async Task SendEmaiAsync(string recipientEmail, string message)
-	{
-		var emailMessage = new MimeMessage();
-		emailMessage.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
-		emailMessage.To.Add(new MailboxAddress(recipientEmail, recipientEmail));
-		emailMessage.Subject = subject;
-		emailMessage.Body = new TextPart("plain")
-		{
-			Text = message
-		};
-
-		using (var client = new SmtpClient())
-		{
-
 		}
 
+		public async Task SendEmaiAsync(string recipientEmail, string message)
+		{
+			var emailMessage = new MimeMessage();
+			emailMessage.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
+			emailMessage.To.Add(new MailboxAddress(recipientEmail, recipientEmail));
+			emailMessage.Subject = subject;
+			emailMessage.Body = new TextPart("plain")
+			{
+				Text = message
+			};
+
+			using (var client = new SmtpClient())
+			{
+				await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.port, SecureSocketOptions.StartTls);
+				await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+				await client.SendAsync(emailMessage);
+				await client.DisconnectAsync(true);
+			}
+
+		}
 	}
 }
