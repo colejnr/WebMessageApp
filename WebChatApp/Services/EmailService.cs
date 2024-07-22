85332f7553c3bd01ba9e17f9c544e51d0cef8985
Najeebvm <najeebvm9562@gmail.com>
2024-07-22 22:47:34 +0530

    Add email message construction

--- WebChatApp/Services/EmailService.cs
+++ WebChatApp/Services/EmailService.cs
@@ -12,6 +12,13 @@ public class Class1
 	public async Task SendEmaiAsync(string recipientEmail, string message)
 	{
 		var emailMessage = new MimeMessage();
+		emailMessage.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
+		emailMessage.To.Add(new MailboxAddress(recipientEmail, recipientEmail));
+		emailMessage.Subject = subject;
+		emailMessage.Body = new TextPart("plain")
+		{
+			Text = message
+		};
 
 	}
 }
