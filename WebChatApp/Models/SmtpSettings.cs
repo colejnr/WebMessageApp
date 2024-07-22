bf76421d276408f08061e350bcfc7d11bb0dfc1e
RobinsSojan <163749930+RobinsSojan@users.noreply.github.com>
2024-07-22 18:20:27 +0100

    Update SmtpSettings

--- WebChatApp/Models/SmtpSettings.cs
+++ WebChatApp/Models/SmtpSettings.cs
@@ -3,5 +3,12 @@ using Org.BouncyCastle.Tls;
 
 namespace ChatApp.Models
 {
-
-}
+    public class SmtpSettings
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
