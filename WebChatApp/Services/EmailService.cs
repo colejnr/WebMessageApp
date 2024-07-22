491f01cf75fcadc285d7d4b8d08051375c4f0de9
colejnr <messicol190@gmail.com>
2024-07-22 20:32:06 +0100

    fix some error

--- WebChatApp/Services/EmailService.cs
+++ WebChatApp/Services/EmailService.cs
@@ -5,37 +5,35 @@ using MimeKit;
 using System.Threading.Tasks;
 using WebChatApp.Models;
 
-
 namespace WebChatApp.Services
 {
-	public class EmailService
-	{
-		private readonly SmtpSettings _smtpSettings;
+    public class EmailService
+    {
+        private readonly SmtpSettings _smtpSettings;
 
-		public EmailService(IOptions<SmtpSettings> smtpSettings)
-		{
-			_smtpSettings = smtpSettings.Value;
-		}
+        public EmailService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
 
-		public async Task SendEmaiAsync(string recipientEmail, string message)
-		{
-			var emailMessage = new MimeMessage();
-			emailMessage.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
-			emailMessage.To.Add(new MailboxAddress(recipientEmail, recipientEmail));
-			emailMessage.Subject = subject;
-			emailMessage.Body = new TextPart("plain")
-			{
-				Text = message
-			};
+        public async Task SendEmailAsync(string recipientEmail, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_smtpSettings.SenderName, _smtpSettings.SenderEmail));
+            emailMessage.To.Add(new MailboxAddress(recipientEmail, recipientEmail));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain")
+            {
+                Text = message
+            };
 
-			using (var client = new SmtpClient())
-			{
-				await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.port, SecureSocketOptions.StartTls);
-				await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
-				await client.SendAsync(emailMessage);
-				await client.DisconnectAsync(true);
-			}
-
-		}
-	}
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(_smtpSettings.Username, _smtpSettings.Password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
 }
