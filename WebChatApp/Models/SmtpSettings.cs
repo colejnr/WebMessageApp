9828565cda810875e4fdab5d91167145945b5398
RobinsSojan <163749930+RobinsSojan@users.noreply.github.com>
2024-07-22 18:21:14 +0100

    changed User to Username

--- WebChatApp/Models/SmtpSettings.cs
+++ WebChatApp/Models/SmtpSettings.cs
@@ -9,6 +9,6 @@ namespace ChatApp.Models
         public int Port { get; set; }
         public string SenderName { get; set; }
         public string SenderEmail { get; set; }
-        public string User { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
