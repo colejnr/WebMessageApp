ad80b97a122827f88a2bfe8f2cf042ac0f10db5b
colejnr <messicol190@gmail.com>
2024-07-22 18:48:11 +0100

    addad email

--- WebChatApp/Models/CreateMessage.cs
+++ WebChatApp/Models/CreateMessage.cs
@@ -8,11 +8,15 @@ namespace WebChatApp.Models
         public string Message { get; set; }
         [Required]
         public string SecretKey { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
 
         public CreateMessage() 
         {
             Message = string.Empty;
             SecretKey = string.Empty;
+            Email = string.Empty;
         }
 
     }
