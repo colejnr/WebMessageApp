55e3a69fdcdc9e9f07a229609b577ed9d2c751ba
Najeebvm <najeebvm9562@gmail.com>
2024-07-22 22:40:55 +0530

    Add SendEmailAsync

--- WebChatApp/Services/EmailService.cs
+++ WebChatApp/Services/EmailService.cs
@@ -4,9 +4,14 @@ public class Class1
 {
 	public class EmailService
 	{
-		private readonly SmtpSettings _smtSettings;
-	public EmailService(IOptions<SmtpSettings> smtSettings)
-			_smtpSettings = smtSettings.Value;
+		private readonly SmtpSettings _smtpSettings;
+	public EmailService(IOptions<SmtpSettings> smtpSettings)
+			_smtpSettings = smtpSettings.Value;
 	
+	}
+	public async Task SendEmaiAsync(string recipientEmail, string message)
+	{
+		var emailMessage = new MimeMessage();
+
 	}
 }
