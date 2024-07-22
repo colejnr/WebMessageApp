74380d7bd9e5f797a039e3131c718fc1688d9844
colejnr <messicol190@gmail.com>
2024-07-22 20:48:19 +0100

    Added created message page

---
+++ WebChatApp/Views/Message/CreatedMessage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebChatApp.Views.Message
+{
+    public class CreatedMessageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
